26d1f53d68e01b76d822b016bdc8456b8023b280
mohammad tavakoli <mohammadtavakolikia66@gmail.com>
2021-10-12 22:45:58 +0330

    like component added

--- src/Component/movies.jsx
+++ src/Component/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {getMovies} from '../services/fakeMovieService'
+import {getMovies} from '../services/fakeMovieService';
+import Like from './common/Like';
 class Movies extends Component {
         constructor(){
           super();
@@ -8,16 +9,23 @@ class Movies extends Component {
           }
         }
         handleDelete = (Movie) =>{
-            console.log("clicked",Movie._id);
             let Movies= this.state.Movies.filter( (m) => m._id !== Movie._id )
             this.setState({
                 Movies
             })
         }
+        
+  handleLike= movie => {
+    const Movies=[...this.state.Movies];
+    const index=Movies.indexOf(movie);
+    Movies[index]={...Movies[index]};
+    Movies[index].liked= !Movies[index].liked;
+    this.setState({Movies});
+  }
     render() { 
         const {length:count}= this.state.Movies;
         if(count === 0) return <h4>There are no movies in database</h4>
-        return <React.Fragment>
+        return (<React.Fragment>
             <h4>Showing {count} movies in the database.</h4>
             <table className="table">
         <thead>
@@ -37,13 +45,19 @@ class Movies extends Component {
                 <td>{Movie.genre.name}</td>
                 <td>{Movie.numberInStock}</td>
                 <td>{Movie.dailyRentalRate}</td>
+                <td>
+                  <Like 
+                  liked={Movie.liked}
+                  onLike={() => this.handleLike(Movie)}
+                  value={Movie}
+                  />
+                  </td>
                 <td><button onClick={() => this.handleDelete(Movie)} className="btn btn-danger">Delete</button></td>
                 </tr>
           })}
         </tbody>
       </table>
-        </React.Fragment>
+        </React.Fragment>)
     }
-}
- 
-export default Movies;
+  }
+  export default Movies;
